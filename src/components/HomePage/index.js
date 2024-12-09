66f53b1ce390e033cc3b246a0685fe06639b2e15
Kushal Vakharia <kushal.workspace123@gmail.com>
2024-12-09 21:50:54 +0530

    file upload done

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Box } from "@mui/material";
 import NavBar from "./NavBar";
+import FileUpload from "./FileUpload";
 
 const HomePage = () => {
   return (
     <Box sx={{ height: "100%", width: "100%" }}>
       <NavBar />
+      <FileUpload />
     </Box>
   );
 };
